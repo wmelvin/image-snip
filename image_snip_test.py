e21e0c3dcc47792e7fee6c78b02814d974279120
Bill Melvin <bill@billmelvin.com>
2021-11-06 19:53:57 -0400

    Replace two separate setup funcs with get_test_opts_and_img.

--- image_snip_test.py
+++ image_snip_test.py
@@ -8,25 +8,28 @@ from textwrap import dedent
 import image_snip
 
 
-def options_file_with_proc(opt_path, process_instruction):
-    d = opt_path / "testopts"
-    d.mkdir()
-    p = d / "test-options.txt"
-    p.write_text(
-        dedent(
-            """
-            output_folder: ./output/tests
-            timestamp_mode: 2
-            {0}
-            ./images/test-1920x1440.jpg
-            """
-        ).format(process_instruction)
-    )
-    assert p.exists()
-    return p
+test_source_image = Path("./images/test-1920x1440.jpg")
 
 
-def options_file_with_proc_and_image(
+# def opt_file_with_proc(opt_path, process_instruction):
+#     d = opt_path / "testopts"
+#     d.mkdir()
+#     p = d / "test-options.txt"
+#     p.write_text(
+#         dedent(
+#             """
+#             output_folder: ./output/tests
+#             timestamp_mode: 2
+#             {0}
+#             {1}
+#             """
+#         ).format(process_instruction, test_source_image)
+#     )
+#     assert p.exists()
+#     return p
+
+
+def get_test_opts_and_img(
     opt_path: Path, process_instruction: str, image_tag: str
 ):
     """
@@ -39,9 +42,8 @@ def options_file_with_proc_and_image(
     test_image_name = (
         f"test-{image_tag}-{datetime.now().strftime('%Y%m%d_%H%M%S_%f')}.jpg"
     )
-    src = Path("./images/test-1920x1440.jpg")
     dst = d / test_image_name
-    shutil.copyfile(src, dst)
+    shutil.copyfile(test_source_image, dst)
 
     opt_file = d / "test-options.txt"
     opt_file.write_text(
@@ -63,63 +65,75 @@ def options_file_with_proc_and_image(
 
 def test_crop_to_box(tmp_path):
     """Test crop_to_box(x1, y1, x2, y2)."""
-    p = options_file_with_proc(tmp_path, "crop_to_box(200, 100, 900, 500)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(
+        tmp_path, "crop_to_box(200, 100, 900, 500)", "crop_to_box"
+    )
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
 
 def test_crop_larger_than_source(tmp_path):
     """Test specifying cropped size larger than the source image size."""
-    opt_path, expect_image = options_file_with_proc_and_image(
+    opt, img = get_test_opts_and_img(
         tmp_path, "crop_from_left_top(2000, 1800)", "larger"
     )
-    args = ["image_snip.py", "--options-file", str(opt_path)]
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
     # Should keep source image size, not scale to larger size.
     expected_size = (1920, 1440)
-    assert Image.open(expect_image).size == expected_size
+    assert Image.open(img).size == expected_size
 
 
 def test_crop_zoom(tmp_path):
-    p = options_file_with_proc(tmp_path, "crop_zoom(800, 800)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(tmp_path, "crop_zoom(800, 800)", "zoom")
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
 
 def test_crop_center(tmp_path):
-    p = options_file_with_proc(tmp_path, "crop_from_center(1024, 768)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(
+        tmp_path, "crop_from_center(1024, 768)", "center"
+    )
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
 
 def test_crop_left_top(tmp_path):
-    p = options_file_with_proc(tmp_path, "crop_from_left_top(640, 480)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(
+        tmp_path, "crop_from_left_top(640, 480)", "left_top"
+    )
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
 
 def test_crop_right_top(tmp_path):
-    p = options_file_with_proc(tmp_path, "crop_from_right_top(640, 480)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(
+        tmp_path, "crop_from_right_top(640, 480)", "right_top"
+    )
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
 
 def test_crop_left_bottom(tmp_path):
-    p = options_file_with_proc(tmp_path, "crop_from_left_bottom(640, 480)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(
+        tmp_path, "crop_from_left_bottom(640, 480)", "left_bottom"
+    )
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
 
 def test_crop_right_bottom(tmp_path):
-    p = options_file_with_proc(tmp_path, "crop_from_right_bottom(640, 480)")
-    args = ["image_snip.py", "--options-file", str(p)]
+    opt, img = get_test_opts_and_img(
+        tmp_path, "crop_from_right_bottom(640, 480)", "right_bottom"
+    )
+    args = ["image_snip.py", "--options-file", str(opt)]
     result = image_snip.main(args)
     assert result == 0
 
@@ -142,9 +156,9 @@ def test_options(tmp_path):
             crop_from_left_bottom(960, 480)
             crop_from_right_bottom(640, 480)
 
-            ./images/test-1920x1440.jpg
+            {0}
             """
-        )
+        ).format(test_source_image)
     )
     assert p.exists()
 
