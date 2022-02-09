2f1a08a201b38ed150d63583ea7895d622033a68
Bill Melvin <bill@billmelvin.com>
2022-02-09 15:10:15 -0500

    Add animated GIF output and overwrite option.

--- image_snip.py
+++ image_snip.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
+import io
 import sys
 
+from collections import namedtuple
 from datetime import datetime
 from pathlib import Path
 from PIL import Image
 
 
-app_version = "211227.1"
+app_version = "220209.1"
 
 pub_version = "0.1.dev1"
 
 app_label = f"image_snip.py version {pub_version} (mod {app_version})"
+
+
+AppOptions = namedtuple(
+    "AppOptions",
+    "proc_list, image_paths, output_dir, timestamp_mode, gif_ms, do_overwrite",
+)
 
 
 def get_new_size_zoom(current_size, target_size):
@@ -169,6 +177,17 @@ def get_target_size(proc, current_size):
     return result
 
 
+def extract_gif_param(proc: str):
+    """
+    Extracts the parameter for an animated GIF, currently just the frame
+    display duration in milliseconds, from a string that ends with an
+    integer, in parentheses.
+    """
+    a = proc.strip(")").split("(")
+    assert len(a) == 2
+    return int(a[1])
+
+
 def extract_target_box(proc: str):
     """
     Extracts target box as a tuple of 4 integers (x1, y1, x2, y2) from
@@ -209,6 +228,14 @@ def get_args(argv):
         + "instructions and image file names, one per line.",
     )
 
+    ap.add_argument(
+        "--overwrite",
+        dest="do_overwrite",
+        action="store_true",
+        help="Overwrite existing output files. By default, existing files are "
+        + "not replaced.",
+    )
+
     return ap.parse_args(argv[1:])
 
 
@@ -225,7 +252,7 @@ def get_opt_str(opt_line: str) -> str:
     return a[1].strip()
 
 
-def get_opts(args):
+def get_opts(args) -> AppOptions:
     opt_file = args.opt_file
     if opt_file is None:
         sys.stderr.write("ERROR: No options file specified.\n")
@@ -241,6 +268,7 @@ def get_opts(args):
     proc_list = []
     output_dir = ""
     timestamp_mode = 0
+    gif_ms = 0
 
     error_list = []
     with open(opt_file, "r") as f:
@@ -250,6 +278,9 @@ def get_opts(args):
                 if s.startswith("crop_") and s.endswith(")"):
                     #  Process instruction.
                     proc_list.append(s)
+                elif s.startswith("animated_gif(") and s.endswith(")"):
+                    #  Instruction to make an animated GIF.
+                    gif_ms = extract_gif_param(s)
                 elif s.startswith("output_folder:"):
                     #  Output folder/directory option.
                     output_dir = get_opt_str(s)
@@ -292,7 +323,62 @@ def get_opts(args):
             sys.exit(1)
         output_dir = str(p)
 
-    return (proc_list, image_paths, output_dir, timestamp_mode)
+    opts = AppOptions(
+        proc_list,
+        image_paths,
+        output_dir,
+        timestamp_mode,
+        gif_ms,
+        args.do_overwrite,
+    )
+
+    return opts
+
+
+def make_gif(gif_ms, image_list):
+    #  Use the last file in the list as the basis for the animated GIF
+    #  file name.
+    last_file = image_list[-1]
+    gif_path = Path(last_file).with_suffix(".gif")
+
+    new_img = None
+    frames = []
+    first = True
+
+    for file_name in image_list:
+        print(f"Reading '{Path(file_name)}'")
+
+        img = Image.open(file_name)
+        # print(img.format, img.size, img.mode)
+
+        mem_buf = io.BytesIO()
+
+        #  Use Image.save to convert to GIF format in-memory.
+        img.save(mem_buf, format="GIF")
+
+        img = Image.open(mem_buf)
+        # print(img.format, img.size, img.mode)
+
+        if first:
+            first_size = img.size
+            new_img = img
+            first = False
+        else:
+            if img.size != first_size:
+                img.resize(first_size)
+            frames.append(img)
+
+    print(f"Writing '{gif_path}'")
+
+    if new_img is not None:
+        new_img.save(
+            str(gif_path),
+            format="GIF",
+            append_images=frames,
+            save_all=True,
+            duration=gif_ms,
+            loop=0,
+        )
 
 
 def main(argv):
@@ -300,21 +386,24 @@ def main(argv):
 
     args = get_args(argv)
 
-    proc_list, image_paths, output_dir, timestamp_mode = get_opts(args)
+    opts = get_opts(args)
 
-    if len(output_dir) == 0:
+    if len(opts.output_dir) == 0:
         #  Default to a new directory under the first image files's parent.
-        dt = datetime.now().strftime('%Y%m%d_%H%M%S')
-        out_path = image_paths[0].parent / f"crop_{dt}"
+        dt = datetime.now().strftime("%Y%m%d_%H%M%S")
+        out_path = opts.image_paths[0].parent / f"crop_{dt}"
         assert not out_path.exists()
         out_path.mkdir()
     else:
         #  If output_dir is specified it must already exist.
-        out_path = Path(output_dir)
+        out_path = Path(opts.output_dir)
 
     assert out_path.exists()
 
-    for image_path in image_paths:
+    # gif_ms = 0
+    gif_images = []
+
+    for image_path in opts.image_paths:
         print(f"Reading '{image_path}'")
 
         src = Image.open(image_path)
@@ -323,7 +412,7 @@ def main(argv):
 
         img.paste(src, (0, 0))
 
-        for proc in proc_list:
+        for proc in opts.proc_list:
             if proc.startswith("crop_from_center"):
                 target_size = get_target_size(proc, img.size)
                 crop_box = crop_box_center(img.size, target_size)
@@ -361,6 +450,10 @@ def main(argv):
                 crop_box = get_target_box(proc, img.size)
                 img = img.crop(crop_box)
 
+            elif proc.startswith("crop_to_box("):
+                crop_box = get_target_box(proc, img.size)
+                img = img.crop(crop_box)
+
             else:
                 sys.stderr.write(
                     "ERROR: Unknown process instruction in options file:\n"
@@ -368,13 +461,26 @@ def main(argv):
                 )
                 sys.exit(1)
 
-        file_name = get_output_name(out_path, image_path, timestamp_mode)
+        file_name = get_output_name(out_path, image_path, opts.timestamp_mode)
         print(f"Saving '{file_name}'")
 
-        #  TODO: Overwrite option?
-        assert not Path(file_name).exists()
+        p = Path(file_name)
+        if p.exists():
+            if opts.do_overwrite:
+                p.unlink()
+            else:
+                sys.stderr.write(
+                    "ERROR: Cannot replace exising file:\n" + f"'{p}'\n"
+                )
+                sys.exit(1)
 
         img.save(file_name)
+
+        if 0 < opts.gif_ms:
+            gif_images.append(file_name)
+
+    if 0 < len(gif_images):
+        make_gif(opts.gif_ms, gif_images)
 
     return 0
 
